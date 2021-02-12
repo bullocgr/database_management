520c3115577ea1c864f89632c4a3b4afe815de76
Bullock <bullocgr@oregonstate.edu>
2021-02-11 18:36:17 -0800

    Updated main

--- assignment3/main.cpp
+++ assignment3/main.cpp
@@ -2,22 +2,31 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <bitset>
+#include <cstdlib>
+#include <bits/stdc++.h> 
 
 using namespace std;
 
 struct Emp {
-	string id, name, bio, manager;
+	string id; //8 bytes
+	string name; // <= 200 bytes
+	string bio; // <= 500 bytes
+	string manager; //8 bytes
+	//will always be < 1016 bytes tho
 };
 
 vector<Emp> readFromFile(string );
+void hash(string );
+string intToBytes(int );
+
 
 int main(){
 
 
 	vector<Emp> data = readFromFile("Employees.csv");
-	for(int i = 0; i < 50; i++) {
-		cout << i << ": " << data[i].id << endl;
-	}
+	hash(data[0].id);
+	
 	return 0;
 }
 
@@ -36,10 +45,45 @@ vector<Emp> readFromFile(string fileName) {
 		getline(file, emp.manager, '\n');
 
 		data.push_back(emp);
-
 	}
 
 	return data;
+}
+
+void hash(string id) {
+	int empId = 0;
+	istringstream ss(id);
+	ss >> empId;
+
+	int temp;
+
+	temp = intToBytes(empId);
+
+	for(int i = 0; i < 2; i++) {
+		cout << temp[i];
+	}
+
+
+}
+
+int intToBytes(int paramInt) {
+	int num = n;
+	int dec_value = 0;
+
+	// Initializing base value to 1, i.e 2^0
+	int base = 1;
+
+	int temp = num;
+	while (temp) {
+		int last_digit = temp % 10;
+		temp = temp / 10;
+
+		dec_value += last_digit * base;
+
+		base = base * 2;
+	}
+
+	return dec_value;
 }
 
 
